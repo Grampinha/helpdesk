cca186dacb4cd2f5484f2f4e6e4be4a9c4ce0e3c
Anderson Grampinha <andersongrampinha@gmail.com>
2023-08-03 17:34:48 -0300

    Validation in new user creation fields

--- src/main/java/com/grampinha/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/grampinha/helpdesk/resources/TecnicoResource.java
@@ -18,6 +18,8 @@ import com.grampinha.helpdesk.domain.Tecnico;
 import com.grampinha.helpdesk.domain.dtos.TecnicoDTO;
 import com.grampinha.helpdesk.services.TecnicoService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
@@ -39,7 +41,7 @@ public class TecnicoResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO) {
+	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
 
 		Tecnico newObj = service.create(objDTO);
 		URI iri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(newObj.getId()).toUri();
